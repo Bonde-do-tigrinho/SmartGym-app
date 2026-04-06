79e91b75f8d787b546e8c419003f80c93189a7e4
RAUL DOS SANTOS ARAUJO <122099251+R4ULz@users.noreply.github.com>
2026-04-06 09:01:47 -0300

    feat: Adicao da separacao de roles para telas de usuarios

--- composeApp/src/commonMain/kotlin/org/smartgym/Routes.kt
+++ composeApp/src/commonMain/kotlin/org/smartgym/Routes.kt
@@ -1,8 +1,16 @@
 package org.smartgym
 
 sealed class Screen(val route: String){
-    object Home : Screen("home")
+
+    //Rotas do aluno
+    object HomeAluno : Screen("home_aluno")
     object Aparelhos: Screen("aparelhos")
     object Treino: Screen("treino")
     object Pagamentos: Screen("pagamentos")
+
+    //Rotas do professor
+    object HomeProfessor : Screen("home_professor")
+
+    //Rotas do admin
+    object HomeAdmin: Screen("home_admin")
 }
