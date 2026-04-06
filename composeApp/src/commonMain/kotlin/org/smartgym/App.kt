9535b261eb6f3a878c192d3e12b5175a510f9a26
Leandro Rodrigues <78665194+LeandroRodrigues061@users.noreply.github.com>
2026-04-06 09:29:00 -0300

    Update HomeScreen Aluno

--- composeApp/src/commonMain/kotlin/org/smartgym/App.kt
+++ composeApp/src/commonMain/kotlin/org/smartgym/App.kt
@@ -2,14 +2,13 @@ package org.smartgym
 
 import androidx.compose.runtime.*
 import androidx.navigation.compose.rememberNavController
-import org.smartgym.Screens.HomeScreen
 import org.smartgym.theme.AppTheme
 
 @Composable
 fun App() {
 
     var DarkTheme by remember { mutableStateOf(false) }
-    val userRole = UserRole.ADMIN
+    val userRole = UserRole.ALUNO
     AppTheme() {
         AppNavigation(
             userRole = userRole,
