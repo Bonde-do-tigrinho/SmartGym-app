b85b79dbf6628ea66996e779072bae49f9f9fba3
Miguel Lemos Ramos <bzats91@gmail.com>
2026-04-17 20:54:13 -0300

    Ajustado ApiClient apra rodar no emulador tambem

--- composeApp/src/commonMain/kotlin/org/smartgym/network/ApiClient.kt
+++ composeApp/src/commonMain/kotlin/org/smartgym/network/ApiClient.kt
@@ -4,13 +4,19 @@ import io.ktor.client.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
+import org.smartgym.getPlatform
 
 object ApiClient {
 
     // Android Emulador → 10.0.2.2
     // Web/Desktop → localhost:8080
     //android fisico -> http://192.168.x.x:8080
-    private const val BASE_URL = "http://localhost:8080"
+    private val BASE_URL: String
+        get() = if (getPlatform().name.startsWith("Android")) {
+            "http://10.0.2.2:8080"
+        } else {
+            "http://localhost:8080"
+        }
 
     val client = HttpClient {
         install(ContentNegotiation) {
@@ -22,5 +28,8 @@ object ApiClient {
         }
     }
 
-    fun getUrl(endpoint: String) = "$BASE_URL$endpoint"
+    fun getUrl(endpoint: String): String {
+        val normalizedEndpoint = if (endpoint.startsWith("/")) endpoint else "/$endpoint"
+        return "$BASE_URL$normalizedEndpoint"
+    }
 }
