cc61e612cf3b3a50065683135a713191c47bee2c
RAUL DOS SANTOS ARAUJO <122099251+R4ULz@users.noreply.github.com>
2026-05-19 13:14:41 -0300

    atualizacao versao kotlin

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
-    kotlin("plugin.serialization") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.21"
 }
 
 compose.desktop {
